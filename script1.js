4f3312e0c4ac7bd6a9da817a012038b4debc2ff8
Riverr-OM <1460115420@qq.com>
2021-04-10 21:44:02 +0800

    puzzle

---
+++ script1.js
@@ -0,0 +1,34 @@
+function outside(){
+  var x = document.getElementById('1');
+  x.style.color = 'black';
+}
+
+function breakdown(){
+  var x = document.getElementById('2');
+  x.style.color = 'black';
+}
+
+function wife(){
+  var x = document.getElementById('3');
+  x.style.color = 'black';
+}
+
+function blood(){
+  var x = document.getElementById('4');
+  x.style.color = 'black';
+}
+
+function stranger(){
+  var x = document.getElementById('5');
+  x.style.color = 'black';
+}
+
+function husband(){
+  var x = document.getElementById('6');
+  x.style.color = 'black';
+}
+
+function button(){
+  var x = document.getElementById('truth');
+  x.style.color = 'black';
+}
